0425e9fd84eadaa41393c7ef37b80aa837ba7498
Tom Magdaci <71394850+TomMagdaci@users.noreply.github.com>
2021-12-09 21:14:19 +0200

    update to fully restful

--- controller/index.js
+++ controller/index.js
@@ -17,31 +17,7 @@ app.use(express.static(('../view')))
 
 app.get("/", (req,res) => {
     res.sendFile("./index.html");
-}) 
-
-const jsonToElement = (data) => {
-  let col = ["row number", "correlated features"];
-  let table = document.createElement('table');
-  table.setAttribute("style", "border:5px solid black;border-collapse:collapse;width:100%;padding:8px;text-align:center;font-size:25px;")
-  let tr = table.insertRow(-1);
-  for (let i = 0; i < col.length; i++) {
-    let th = document.createElement("th");
-    th.innerHTML = col[i];
-    th.setAttribute("style", "border:5px solid black;text-align:center;font-size:35px;font-family: 'Oxygen', sans-serif;")
-    tr.appendChild(th);
-  }
-  for(const [key, value] of Object.entries(data)) {
-    tr = table.insertRow(-1);
-    tr.setAttribute("style", "border:5px solid black;border-collapse:collapse;width:100%;padding:8px;text-align:center;font-size:25px;background-color: #f2f2f2;")
-    let tabCell1 = tr.insertCell(-1);
-    tabCell1.setAttribute("style", "border:5px solid black;text-align:center;font-size:25px;font-family: 'Oxygen', sans-serif;")
-    tabCell1.innerHTML = key;
-    let tabCell2 = tr.insertCell(-1);
-    tabCell2.setAttribute("style", "border:5px solid black;text-align:center;font-size:25px;font-family: 'Oxygen', sans-serif;")
-    tabCell2.innerHTML = value;
-  }
-  return table.outerHTML;
-  }
+}) ;
 
 app.post("/detect",(req,res)=>  {
   var myAlgorithm = req.body.myAlgorithm //anomaly detector kind
@@ -54,7 +30,8 @@ app.post("/detect",(req,res)=>  {
     promise1.then((value) => {
       console.log(value);
       //generate view...
-      res.write(jsonToElement(value));
+      //res.write(jsonToElement(value));
+      res.write(JSON.stringify(value))
       res.end();
     });
   }
